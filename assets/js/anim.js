98a602c617a9c03eb98f8ee56e26afb94bee8133
Debin Babykutty <96095584+Debinb@users.noreply.github.com>
2024-06-23 05:20:50 +0000

    reverted

--- assets/js/anim.js
+++ assets/js/anim.js
@@ -1,20 +1,20 @@
-(function($) {
+// (function($) {
 
-var pathEls = document.querySelectorAll('path');
-			for (var i = 0; i < pathEls.length; i++) 
-				{
-					var pathEl = pathEls[i];
-					var offset = anime.setDashoffset(pathEl);
-					pathEl.setAttribute('stroke-dashoffset', offset);
-					anime({
-						targets: pathEl,
-						strokeDashoffset: [offset, 0],
-						duration: 7000,
-						delay: anime.random(0, 2000),
-						loop: true,
-						direction: 'alternate',
-						easing: 'linear',
-						autoplay: true
-					});
-				}
-            })
+// var pathEls = document.querySelectorAll('path');
+// 			for (var i = 0; i < pathEls.length; i++) 
+// 				{
+// 					var pathEl = pathEls[i];
+// 					var offset = anime.setDashoffset(pathEl);
+// 					pathEl.setAttribute('stroke-dashoffset', offset);
+// 					anime({
+// 						targets: pathEl,
+// 						strokeDashoffset: [offset, 0],
+// 						duration: 7000,
+// 						delay: anime.random(0, 2000),
+// 						loop: true,
+// 						direction: 'alternate',
+// 						easing: 'linear',
+// 						autoplay: true
+// 					});
+// 				}
+//             })
